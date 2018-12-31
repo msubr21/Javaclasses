736f806bd1feff9e40fbe4f3141e4dcb28ee00a1
chaithanya <chaithanyakumar.karuna@charter.com>
2018-12-31 09:54:47 -0700

    THis is my commit to Murali git

---
+++ classone/src/main/java/com/javapros/classone/ChaithanyaSample.java
@@ -0,0 +1,17 @@
+package com.javapros.classone;
+
+/**
+ * This class to test my commit to Murali git from my local.
+ * @author p2799013
+ *
+ */
+public class ChaithanyaSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hi this is my commit to your git");
+	}
+
+}
