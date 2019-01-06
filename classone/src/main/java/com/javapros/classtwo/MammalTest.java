0ed952a6cb3f341859777f69435bf451ad29f5c9
M1036289 <M1036289@A2ML30894.mindtree.com>
2019-01-07 01:33:47 +0530

    Assignment Programs

---
+++ classone/src/main/java/com/javapros/classtwo/MammalTest.java
@@ -0,0 +1,54 @@
+package com.javapros.classtwo;
+
+class Mammal {
+
+	String name;
+	int age;
+	String gender;
+	String foodhabit;
+	String mammarygland;
+	String hairandfur;
+	static String category;
+	static {
+		category = "Mammal";
+	}
+
+	//Default constructor
+	public Mammal() {
+	}
+	//constructor with selected fields for creating instance
+	public Mammal(String name, int age, String gender, String foodhabit) {
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.foodhabit = foodhabit;
+	}
+
+		
+	public Mammal(String name, int age, String gender) {
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+	}
+	void printMammalDetails() {
+		//String namex = "Gomathi";
+		//name = namex;
+		System.out.println("name::" + name +"Age::" +age  +"Gender::" +gender +"foodhabit::" +foodhabit);
+
+	}
+}
+
+public class MammalTest {
+
+	public static void main(String[] args) {
+		Mammal cow = new Mammal();
+		cow.name = "Gomathi";
+		Mammal dog = new Mammal("Jimmy",5, "Male","Non-Veg");
+		Mammal human = new Mammal("Rajni",65,"Male");
+		cow.printMammalDetails();
+		dog.printMammalDetails();
+		human.printMammalDetails();
+
+	}
+
+}
